af403c45c5c2b990591f38b6ba610c86f1e0fdb2
DrMadhushan <drmadhushan@gmail.com>
2021-05-27 22:41:31 +0530

    Updated routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { SigninComponent } from './signin/signin.component';
 import { EditProfileComponent } from './edit-profile/edit-profile.component';
@@ -7,12 +8,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full' },
   {path: 'home', component: HomeComponent},
   {path: 'student-profile', component: StudentProfileComponent},
   {path: 'edit-profile', component: EditProfileComponent},
   {path: 'signin', component: SigninComponent},
+  {path: 'reset-password', component: ResetPasswordComponent},
   {path: '**', component: PageNotFoundComponent}
 ]
 
