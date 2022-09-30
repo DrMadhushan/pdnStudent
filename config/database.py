d18594b365185726f0b7d3058a89b54c216061fa
DrMadhushan <dev.drmad@gmail.com>
2022-09-30 13:36:36 +0530

    add database configurations

---
+++ config/database.py
@@ -0,0 +1,14 @@
+HOST = "mongodb://localhost:27017"
+DATABASE_NAME = 'pdnPeople'
+STUDENTS_COLL = 'students'
+STAFF_COLL = 'staff'
+
+STUDENT_SCHEMA = {
+    "student_name" : "name",
+    "roll_no" : "roll_no",
+    "batch" : "batch",
+    "faculty" : "faculty",
+    "department" : "department",
+    "interests" : "interests",
+    "img_link" : "img"
+}
