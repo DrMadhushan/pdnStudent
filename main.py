451b39b1b187192a322cbec86e5ff2e8fef170a4
DrMadhushan <dev.drmad@gmail.com>
2022-10-01 15:27:35 +0530

    create update profile function route

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # main.py
 
-from fastapi import Depends, FastAPI, status
+from fastapi import Depends, FastAPI, status, Form
 from fastapi.security import OAuth2PasswordBearer
 import database as db
 from pydantic import BaseModel
@@ -16,27 +16,34 @@ async def getStudents():
     # Get all students name and img link
     return await db.getAllStudents()
 
-@app.get("/students/{roll_no}")
-async def getStudentInfo(roll_no: str):
-    # returns the requested student's details
-    return await db.getStudentInfo(roll_no)
-
 @app.get("/students/{faculty}")
 async def getFacultyStudents(faculty: str):
     # Get all students name and img link in a given faculty
     return await db.getFacultyStudents(faculty)
-
-@app.get("/{faculty}/students/{roll_no}")   # , status_code=status.HTTP_202_ACCEPTED
-async def getStudentInfo(roll_no: str, faculty: str):
-    # returns the requested student's details
-    return db.getStudentInfo(roll_no, faculty)
 
 @app.get("/students/{faculty}/{department}")
 async def getDepartmentStudents(faculty: str, department: str):
     # Get all students name and img link in a given department and faculty 
     return await db.getDepartmentStudents(faculty, department)   
 
-@app.get("/{faculty}/{department}/students/{roll_no}")
-async def getStudentInfo(roll_no: str, faculty: str, department: str):
+@app.get("/student/view/{roll_no}")
+async def getStudentInfo(roll_no: str):
     # returns the requested student's details
-    return await db.getStudentInfo(roll_no, faculty, department)
+    return await db.getStudentInfo(roll_no)
+
+@app.post("/student/edit/{roll_no}")
+async def updateStudentInfo(roll_no: str, new_name: str = Form()):
+    # Update the student details
+    return await db.updateStudentInfo(roll_no, new_name)
+
+@app.get("/controls/admin/profile_updates")
+async def getAllProfileUpdateRequests():
+    return await db.getAllProfileUpdateRequests()
+
+
+'''
+@app.get("/students/{roll_no}")
+async def getStudentInfo(roll_no: str):
+    # returns the requested student's details
+    return await db.getStudentInfo(roll_no)
+'''
