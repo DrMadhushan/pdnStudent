0aabea004a6755a4b34c603668eb09a02dc1393f
DrMadhushan <dev.drmad@gmail.com>
2022-10-02 10:06:55 +0530

    update jwt secret key and expire time

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ from passlib.hash import bcrypt
 
 import database as db
 
-JWT_SECRET = "secret_key"
+JWT_SECRET = "9afd9e37-19d5-4159-8c04-906f5b4c648e"
 JWT_ALGORITHM = "HS256"
-JWT_TOKEN_EXPIRE_MINUTES = 5
+JWT_TOKEN_EXPIRE_MINUTES = 30
 
 app = FastAPI()
 
